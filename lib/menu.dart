1e5591a0b13e596382c33e2b9f6013f1510eae87
nurfajarismail <62985196+nurfajarismail@users.noreply.github.com>
2021-02-13 19:06:00 +0700

    add bottomnavigation page

---
+++ lib/menu.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatefulWidget {
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Login',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Fisika',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Konversi',
+          ),
+        ],
+      ),
+    );
+  }
+}
